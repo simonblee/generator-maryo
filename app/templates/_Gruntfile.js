36993588aa7871dea58d88124e79165e2e462721
Simon Blee <simblee@gmail.com>
2013-09-14 09:42:27 +0800

    Change server port to 8888 to fix #1

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -48,7 +48,7 @@ module.exports = function (grunt) {
         },
         connect: {
             options: {
-                port: 9000,
+                port: 8888,
                 // change this to '0.0.0.0' to access the server from outside
                 hostname: 'localhost'
             },
