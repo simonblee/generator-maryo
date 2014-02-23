a0b640d59775d8f12a61c1774eddce10904cae21
Simon Blee <simblee@gmail.com>
2014-02-24 04:32:56 +0800

    Patch for #13

--- app/templates/_config.js
+++ app/templates/_config.js
@@ -4,7 +4,7 @@ requirejs.config({
         backbone: '../bower_components/backbone/backbone',<% if (templateFormat === 'dust') { %>
         dust: '../bower_components/dustjs-linkedin/lib/dust',
         dustHelpers: '../bower_components/dustjs-linkedin-helpers/lib/dust-helpers',
-        dustMarionette: '../bower_components/marionette-dust/src/backbone.marionette.dust',<% } %>
+        dustMarionette: '../bower_components/marionette-dust/src/amd/backbone.marionette.dust',<% } %>
         jquery: '../bower_components/jquery/jquery',
         marionette: '../bower_components/marionette/lib/core/amd/backbone.marionette',
         underscore: '../bower_components/lodash/lodash',
